40f4ebd53e59de3ade2217f09e438bd3307bee47
Sutton <sut109@csiro.au>
2019-09-19 16:44:27 +1000

    demo

---
+++ scripts/demos.R
@@ -0,0 +1,89 @@
+1+100
+1+
+  +2
+(3+5)*2
+3+ 5*(2^2)
+2/10000
+5e3 #demonstrating scientific notation
+#equality
+#one equals one
+1==1 #one equals one (must put notes after the code OR on the line before the code)
+1!=2
+1!=1
+1<2
+1>10
+2<+5
+2<=2
+pi
+pi == 3.141593
+1.25 == 1.25
+
+
+#storing a value
+x <- 1/40
+x # will now return the value for x
+y <- 1000
+y
+x + y
+rm(x)
+x
+y <- 10
+log(x)
+log(1/40)
+
+
+x<- x+1
+x
+X
+
+
+x <- NA
+x
+x == NA
+is.na(x)
+
+
+z <- na
+is.na(x)
+
+
+my_data <- 1
+mydata
+my.data
+my_data
+
+
+min_height
+max.height
+
+min_height <- 20
+max.height <- 30
+_age <- 27
+.mass <- 60
+MaxLength <- 100
+min-length <- 20
+2widths - 40
+celsius2kelvin <- 20
+
+
+#vectorisation
+
+1:5 # a set values all the same kind of number is a particular order
+2^(1:5)
+
+my_vector <- c(4,2,7,2,3)
+2^my_vector
+
+#provides a list of all teh values loaded in my environment
+ls()
+ls #if you leave off the brackets R will print out all the code related to the functions. THis can offen happen..
+
+rm(max.height)
+ls()
+
+installed.packages()
+installed.packages("tidyverse")
+ zx
+ 
+ library(tidyverse)
+ 
