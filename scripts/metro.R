0bd9c7e9f9f943339a8c114abdd7c5d2aceb5edd
Sutton <sut109@csiro.au>
2019-09-26 12:21:35 +1000

    commit code for question 2

--- scripts/metro.R
+++ scripts/metro.R
@@ -4,19 +4,50 @@ bom_data
 bom_st <- read_csv("data/BOM_stations.csv")
 bom_st
 
-#Challenge 
+# Challenge: Putting it all together
+
+# Question 1 (Sept 25)
 # For each station, how many days have a minimum temperature, 
-#a maximum temperature and 
+# a maximum temperature and 
 # a rainfall measurement recorded
 
-bom_sep <- separate(bom_data, col = Temp_min_max, into = c("min", "max"), sep = "/") # sep the min/max column
+# sep the min/max column
+bom_sep <- separate(bom_data, col = Temp_min_max, into = c("min", "max"), sep = "/") 
 
+
+# filter to include only stations with data for min/max/Rainfall data
 bom_sep %>% 
-  filter(min != "-", max !="-", Rainfall !="-")# filter to include only stations with data for min/max/Rainfall data
+  filter(min != "-", max !="-", Rainfall !="-")
 
+# assign a new talbe to data and count the number of station
 Data_count_ofMinMaxRainfall <- bom_sep %>%
   count(Station_number)
-rm(Data_count)  
+
+count(Data_count_ofMinMaxRainfall) # counting the number of row for stations with data
+
+Bom_sumStation <- bom_sep %>% 
+  group_by(Station_number)
+
+count(Bom_sumStation)
 
 
 
+
+# Question 2  Which month saw the lowest average daily temperature difference? (Sept 26)
+
+Bom_Diff <- bom_sep %>% 
+  mutate (min = as.numeric(min)) %>% 
+  mutate (max = as.numeric(max)) %>% 
+  mutate (Rainfall = as.numeric(Rainfall)) %>% 
+  mutate(temp_diff =min - max) %>% 
+  filter (temp_diff != "NA")
+  
+Bom_Diff %>% 
+  arrange(desc(temp_diff)) %>% 
+  filter(temp_diff <0)
+
+ 
+
+  
+
+         
